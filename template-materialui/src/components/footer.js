b56d5ae361febae5a6ace5ce2f8091516de8cd11
Amanda M <amandasmedeiros@outlook.com.br>
2024-06-02 12:19:10 -0300

    first commit

---
+++ template-materialui/src/components/footer.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+ 
+function Footer() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <footer>
+                <p>🎓Feito para a disciplina Prática Integradora de Tecnologias Disruptivas</p>
+            </footer>
+        </Box>
+    );
+}
+ 
+export default Footer;
